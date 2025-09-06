f14aab839afbcbf6c6dcddc23f855f4d45d66226
moshaosama <mohamedOSFekry@gmail.com>
2025-09-06 20:20:07 +0300

    Update sendMessage.service.ts: تعديل منطق إرسال الرسائل

---
+++ Services/sendMessage.service.ts
@@ -0,0 +1,24 @@
+export const sendMessage = async (message: string) => {
+  const response = await fetch(
+    "https://api.groq.com/openai/v1/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "llama-3.3-70b-versatile",
+        messages: [
+          {
+            role: "user",
+            content: message as string,
+          },
+        ],
+        max_tokens: 200,
+      }),
+    }
+  );
+
+  return response.json();
+};
