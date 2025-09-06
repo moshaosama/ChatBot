342370c157e1e7483be582dc368e73a05e8f593c
moshaosama <mohamedOSFekry@gmail.com>
2025-09-06 17:26:50 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Welcome from "../Welcome/page";
+import Header from "../Components/Header";
+import { IoMdSend } from "react-icons/io";
+
+const page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div className="bg-gradient-to-l relative from-blue-200 to-purple-200 h-[100vh]">
+      {/* {isLoading ? <Welcome /> : null} */}
+
+      <>
+        <Header />
+
+        <form className="fixed bottom-0 w-full flex gap-3 justify-center bg-white p-4">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="w-[60pc] p-4 rounded-xl bg-[#e7e7e7]"
+            placeholder="Type your message here..."
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 cursor-pointer hover:bg-blue-600 transition-all duration-300 text-white p-4 rounded-xl"
+          >
+            <IoMdSend size={20} />
+          </button>
+        </form>
+      </>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/Components/Header.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+
+const Links = [
+  {
+    name: "Features",
+  },
+  {
+    name: "Pricing",
+  },
+  {
+    name: "Contact",
+  },
+];
+
+const Header = () => {
+  return (
+    <div className="bg-white  shadow-md shadow-gray-400">
+      <div className=" flex mx-20 justify-between items-center">
+        <div className="flex items-center gap-3 py-3">
+          <Image
+            src="/istockphoto-1010001882-612x612-removebg-preview.png"
+            alt="Chatbot.png"
+            width={70}
+            height={70}
+          />
+          <h1 className="text-xl font-bold">ChatBot</h1>
+        </div>
+        <div>
+          <div className="flex gap-5  items-center text-lg font-bold">
+            {Links?.map((link) => (
+              <div>
+                <h1>{link.name}</h1>
+              </div>
+            ))}
+
+            <div className="bg-blue-500 w-32 text-center text-white rounded-xl py-2">
+              get started
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
